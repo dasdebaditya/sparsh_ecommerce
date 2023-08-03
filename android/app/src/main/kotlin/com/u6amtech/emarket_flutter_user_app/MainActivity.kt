0cc30dfc0675abaf080069c0f8afa38af6f4bd1a
Debaditya Das <dasdebaditya@outlook.com>
2023-08-04 00:01:56 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/u6amtech/emarket_flutter_user_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.u6amtech.emarket_flutter_user_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+
+}
---
+++ lib/provider/news_provider.dart
@@ -0,0 +1,22 @@
+import 'package:sparsh_user/data/model/response/base/api_response.dart';
+import 'package:sparsh_user/data/repository/news_letter_repo.dart';
+import 'package:sparsh_user/localization/language_constrants.dart';
+import 'package:sparsh_user/view/base/custom_snackbar.dart';
+import 'package:flutter/material.dart';
+
+
+class NewsLetterProvider extends ChangeNotifier {
+  final NewsLetterRepo newsLetterRepo;
+  NewsLetterProvider({@required this.newsLetterRepo});
+
+
+  Future<void> addToNewsLetter(BuildContext context, String email) async {
+    ApiResponse apiResponse = await newsLetterRepo.addToNewsLetter(email);
+    if (apiResponse.response != null && apiResponse.response.statusCode == 200) {
+      showCustomSnackBar(getTranslated('successfully_subscribe', context), context,isError: false);
+    } else {
+      showCustomSnackBar(getTranslated('mail_already_exist', context), context);
+    }
+  }
+  notifyListeners();
+}
